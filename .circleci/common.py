af73748600177b823c259d6348fbbce6972b4d67
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-07-28 20:37:30 +0100

    add back version as they are needed in non edge build

--- .circleci/common.py
+++ .circleci/common.py
@@ -35,7 +35,7 @@ def get_airflow_version(ac_version):
 
 IMAGE_MAP = collections.OrderedDict([
     ("1.10.15-9", ["buster"]),
-    ("2.1.4-9", ["buster"]),
+    ("2.1.4-10-dev", ["buster"]),
     ("2.3.3-2-dev", ["bullseye"]),
     ("main-dev", ["bullseye"]),
 ])
